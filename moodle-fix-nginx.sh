cd29efbacc2030799fc2b5c6dbd850781058b442
Patrick F. Weber <pfw@patrickweber.info>
2026-07-28 15:55:22 +0300

    More Moodle fixes

--- moodle-fix-nginx.sh
+++ moodle-fix-nginx.sh
@@ -4,3 +4,10 @@ for f in /etc/nginx/http.d/default.conf /etc/nginx/conf.d/default.conf; do
     sed -i '/^\s*allow\s\+127\.0\.0\.1\s*;/d' "$f"
     sed -i '/^\s*deny\s\+all\s*;/d' "$f"
 done
+
+CONFIG_PHP="/var/www/html/config.php"
+if [ -f "$CONFIG_PHP" ]; then
+    if ! grep -q "trustedproxies" "$CONFIG_PHP"; then
+        sed -i '/$CFG->reverseproxy/a $CFG->trustedproxies = '"'"'127.0.0.1, 172.0.0.0/8'"'"';' "$CONFIG_PHP"
+    fi
+fi
